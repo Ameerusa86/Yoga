a25b41f08b0e62d81a327cec925bbbd618564112
Ameerusa86 <engamermecha@gmail.com>
2023-04-06 23:58:48 -0400

    -

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,3 +97,16 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: 400,
+});
+sr.reveal(`.home__data, .list__container, .join__content, .footer__container`);
+sr.reveal(`.home__img`, { origin: "bottom" });
+sr.reveal(`.health__image, .routine__images, .follow__img-3`, {
+  origin: "left",
+});
+sr.reveal(`.health__data, .routine__data,.follow__img-3`, { origin: "right" });
+sr.reveal(`.follow__data, .follow__content-1`, { interval: 100 });
